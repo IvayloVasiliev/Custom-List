ae56e9c6f24e2207d08ee0a0c18ae953c87de1a6
Ivailo Vasiliev <ivailo_vasiliev@hotmail.com>
2019-12-23 11:20:06 +0200

    Add project files.

---
+++ CustomList/CustomList.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomList
+{
+    public class CustomList<T> : IEnumerable
+    {
+        private const int initialCapacity = 2;
+
+        private T[] array;
+
+        public CustomList()
+        {
+            array = new T[initialCapacity];
+            Count = 0;
+        }
+
+        public int Count { get; private set; }
+        public T this[int number]
+        {
+            get
+            {
+                // This is invoked when accessing Layout with the [ ].
+                if (number >= 0 && number < Count)
+                {
+                    return array[number];
+                }
+                throw new IndexOutOfRangeException();
+            }
+            set
+            {
+                // This is invoked when assigning to Layout with the [ ].
+                if (number >= 0 && number < Count)
+                {
+                    array[number] = value;
+                }
+                else
+                {
+                    throw new IndexOutOfRangeException();
+                }
+            }
+        }
+
+        public void Add(T item)
+        {
+            if (Count == array.Length)
+            {
+                Resize();
+            }
+
+            array[Count++] = item;
+        }
+
+        public void Remove(T item)
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                if (array[i].Equals(item))
+                {
+                    array[i] = default(T);
+                    Count--;
+                    Shrink(i);
+                }
+            }
+        }
+
+        private void Shrink(int index)
+        {
+            for (int i = index; i < Count; i++)
+            {
+                array[i] = array[i +1];
+            }
+        }
+
+      
+        private void Resize()
+        {
+            T[] newArray = new T[array.Length * 2];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                newArray[i] = array[i];
+            }
+
+            array = newArray;
+
+        }
+        public IEnumerator GetEnumerator()
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                yield return array[i];
+            }
+        }
+    }   
+}
---
+++ CustomList/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace CustomList
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            CustomList<object> customList = new CustomList<object>();
+
+            customList.Add("Gosho");
+            customList.Add("Iv4o");
+            customList.Add("12");
+            customList.Add("10.5");
+            
+            //customList.Remove("Iv4o");
+
+            customList[3] = "Victor";
+
+            for (int i = 0; i < customList.Count; i++)
+            {
+                Console.WriteLine(customList[i]);
+            }
+
+
+        }
+    }
+}
